73bd8a7519a2f0f4be932957f1a15288b89dfd71
Jonah Snider <me@jonahsnider.ninja>
2019-10-31 11:45:36 -0700

    refactor: lowercase Node.js version variable
    Signed-off-by: Jonah Snider <me@jonahsnider.ninja>

--- nodejs.sh
+++ nodejs.sh
@@ -1,5 +1,5 @@
-NODE_VERSION=12
+node_version=12
 
 sudo apt-get install -y build-essential
-curl -sL "https://deb.nodesource.com/setup_$NODE_VERSION.x" | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_$node_version.x" | sudo -E bash -
 sudo apt-get install -y nodejs

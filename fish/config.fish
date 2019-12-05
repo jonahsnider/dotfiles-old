8ff945838c245a1c8d84b3d26e32f2c5b0a32808
Jonah Snider <jonah@jonah.pw>
2019-12-05 10:18:56 -0800

    docs: add comment about GPG_TTY fix
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- fish/config.fish
+++ fish/config.fish
@@ -36,6 +36,7 @@ set -gx DOTNET_CLI_TELEMETRY_OPTOUT true
 # Fix the "gpg: error building skey array: Inappropriate ioctl for device" error
 # Solution from https://github.com/keybase/keybase-issues/issues/1712#issuecomment-141226705
 # This gets piped to /dev/null because it will print environment variables for some reason
+# If this doesn't fix the issue, try adding something like this to /etc/environment
 export GPG_TTY=$tty > /dev/null
 
 # https://github.com/starship/starship
